23cd8e0f7c9e6f8f7c7b9b54c9b550026dcf2d40
BryceDay64 <brycecday23@gmail.com>
2025-05-30 12:37:40 -0500

    Added pygame to display the current velocity

--- SelfSelectedSpeed_v1.py
+++ SelfSelectedSpeed_v1.py
@@ -1,19 +1,38 @@
 import BertecRemoteControl
 from time import sleep
+import pygame
+
+white = (255, 255, 255)
+black = (0, 0, 0)
 
 remote = BertecRemoteControl.RemoteControl()
 res = remote.start_connection()
 print(res)
 
 
+
+
 initialize_velocity = input("Set the initial velocity")
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Velocity')
+font = pygame.font.Font(None, 36)
+text = font.render(initialize_velocity,True, white, black)
 
 params = remote.get_run_treadmill_user_input()
 res = remote.run_treadmill(initialize_velocity, 1, 1, initialize_velocity, 1, 1)
 
 velocity = initialize_velocity
 
+textRect = text.get_rect()
+textRect.center = (320, 240)
+
 while True:
+
+    screen.fill(white)
+    screen.blit(text, textRect)
+
     res = remote.get_force_data()
 
     copy = res['copy']
@@ -34,4 +53,21 @@ while True:
         velocity = 0
         res = remote.run_treadmill(velocity, 1, 1, velocity, 1, 1)
         break
+
+    text = font.render(str(velocity), True, white, black)
+    for event in pygame.event.get():
+
+        # if event object type is QUIT
+        # then quitting the pygame
+        # and program both.
+        if event.type == pygame.QUIT:
+            # deactivates the pygame library
+            pygame.quit()
+
+            # quit the program.
+            quit()
+
+        # Draws the surface object to the screen.
+    pygame.display.update()
+    
     sleep(0.05)
